0204c81198d14070318f4b4a771d175015a56947
Amrit Singh <dev.amritsingh0870@gmail.com>
2023-10-02 01:52:39 +0530

    routes added

--- wrkin_admin/urls.py
+++ wrkin_admin/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from wrkin_admin.views import *
 urlpatterns = [
-                path('login',login),
-                path('index',index),
+                path('login',login,name='login'),
+                path('index',index,name='index'),
 ]
